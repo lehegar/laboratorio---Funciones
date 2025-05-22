7fd68f114d8a55668b2cd9977d1bd6c1a6ad3164
Leticia <lehegar@gmail.com>
2025-05-22 12:27:50 +0200

    Guardado

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ let turnoActual: number = 0;
 
 const numeroDisplay = document.getElementById("numero-turno");
 const botonSiguiente = document.getElementById("siguiente");
+const botonAnterior = document.getElementById("anterior");
+const botonReset = document.getElementById("reset");
 
 function displayActualizado() {
   if (numeroDisplay) {
@@ -14,4 +16,16 @@ function siguiente() {
   displayActualizado();
 }
 
+function anterior() {
+  turnoActual = turnoActual - 1;
+  displayActualizado();
+}
+
+function reset() {
+  turnoActual = 0;
+  displayActualizado();
+}
+
 botonSiguiente.addEventListener("click", siguiente);
+botonAnterior.addEventListener("click", anterior);
+botonReset.addEventListener("click", reset);
